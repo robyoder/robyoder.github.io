81c72560020095c9d443599c1787a5b00318eef3
Rob Yoder <rob@robyoder.com>
2020-01-30 20:10:21 -0700

    Initial commit

---
+++ resources/main-v2.js
@@ -0,0 +1,22 @@
+for (const el of document.getElementsByClassName("obfuscate")) {
+	const text = atob(el.dataset.t);
+	el.textContent = text;
+
+	if (el.classList.contains("type-e")) {
+		// This is an email address
+		el.setAttribute("href", "mailto:" + text);
+	}
+	else if (el.classList.contains("type-t")) {
+		// This is a phone number
+		el.setAttribute("href", "tel:+1" + text.replace(/\D/g, ""));
+	}
+
+	if (el.classList.contains("br")) {
+		// Add a line break afterward
+		const br = document.createElement("br");
+		const next = el.nextSibling;
+		if (next) {
+			el.parentNode.insertBefore(br, next);
+		}
+	}
+}
